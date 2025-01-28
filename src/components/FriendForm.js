94e7137c7aed7f6b4bb4d21b26f1f95db8aee49a
Courtney Johnson <cjandy13@gmail.com>
2025-01-27 18:11:18 -0600

    update

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -7,14 +7,19 @@ export default function FriendForm(props) {
   const onChange = evt => {
     // 🔥 STEP 6 - IMPLEMENT the change handler for our inputs and dropdown
     // a) pull the name of the input from the event object
+    const name = evt.target.name;
     // b) pull the value of the input from the event object
+    const {value} = evt.target;
     // c) use the `update` callback coming in through props
+    update(name, value);
   }
 
   const onSubmit = evt => {
     // 🔥 STEP 7 - IMPLEMENT the submit handler
     // a) don't allow the browser to reload!
+    evt.preventDefault()
     // c) use the `submit` callback coming in through props
+    submit()
   }
 
   return (
@@ -23,15 +28,31 @@ export default function FriendForm(props) {
         {/* ////////// TEXT INPUTS ////////// */}
         {/* ////////// TEXT INPUTS ////////// */}
         {/* ////////// TEXT INPUTS ////////// */}
+
         <label>Username
           {/* 🔥 STEP 3 - Make an input of type `text` for username.
               Controlled inputs need `value` and `onChange` props.
               Inputs render what they're told - their current value comes from app state.
               At each keystroke, a change handler fires to change app state. */}
+          <input 
+            type="text"
+            name="username"
+            placeholder="Type a username ya chump..."
+            value={values.username}
+            onChange={onChange}
+            maxLength="30"
+          />
         </label>
 
         <label>Email
           {/* 🔥 STEP 4 - Make an input of type `email` or `text` for email. */}
+          <input 
+            type="email"
+            name="email"
+            placeholder="Type an email ya chump..."
+            values={values.email}
+            onChange={onChange}
+          />
         </label>
 
         {/* ////////// DROPDOWN ////////// */}
@@ -39,6 +60,12 @@ export default function FriendForm(props) {
         {/* ////////// DROPDOWN ////////// */}
         <label>Role
           {/* 🔥 STEP 5 - Make dropdown for role. */}
+          <select value={values.role} name="role" onChange={onChange}>
+            <option value="">--- Select a Role ---</option>
+            <option value="student">Student</option>
+            <option value="Instructor">Instructor</option>
+            <option value="Alumni">Alumni</option>
+          </select>
         </label>
 
         <div className='submit'>
